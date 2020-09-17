d34859e61e2409045619e1963d0df6bb8044fa0a
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-09-17 11:00:15 +0200

    Added admin overview page

---
+++ lib/pluggy/controllers/admin_controller.ex
@@ -0,0 +1,22 @@
+defmodule Pluggy.AdminController do
+  # import Pluggy.Template, only: [render: 2] #det här exemplet renderar inga templates
+  import Plug.Conn, only: [send_resp: 3]
+  import Pluggy.Template, only: [srender: 1, srender: 2]
+  alias Pluggy.User
+
+  alias Pluggy.Class
+  alias Pluggy.School
+
+
+
+  def index(conn) do
+    session_user = conn.private.plug_session["user_id"]
+    current_user =
+      case session_user do
+        nil -> nil
+        _ -> User.get(session_user)
+      end
+      IO.inspect(Enum.map School.all(), fn(x) -> IO.inspect(x.name) end)
+    send_resp(conn, 200, srender("admin/index", [schools: School.all(), classes: Class.all()]))
+  end
+end

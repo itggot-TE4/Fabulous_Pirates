786f79b0f25eaf946b300171581d51fd269cb718
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-09-21 20:41:33 +0200

    Added doctest to generic module

--- test/pluggy_test.exs
+++ test/pluggy_test.exs
@@ -1,8 +1,9 @@
 defmodule PluggyTest do
   use ExUnit.Case
   doctest Pluggy
+  doctest Pluggy.Generic
 
-  test "greets the world" do
-    assert Pluggy.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Pluggy.hello() == :world
+  # end
 end

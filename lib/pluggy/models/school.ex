9396729d6b4aa6a41266057d5a3b530192bb9313
Jonathan Ronsten <jonathan.ronsten@elev.ga.ntig.se>
2020-09-22 09:43:38 +0200

    Added modules and implemented specific functions

---
+++ lib/pluggy/models/school.ex
@@ -0,0 +1,29 @@
+defmodule Pluggy.School do
+  defstruct(id: nil, school_name: "", img: "")
+
+  require Pluggy.Generic
+  alias Pluggy.School
+
+
+  def get(id) do
+    Pluggy.Generic.get(id, "schools", School)
+  end
+
+  def all() do
+    Pluggy.Generic.all("schools", School)
+  end
+
+  def get_by_user_id(user) do
+    Postgrex.query!(DB, "SELECT Schools.id, school_name
+    FROM Schools
+    JOIN User_School_id ON Schools.id = User_school_id.school_id
+    JOIN Users ON Users.id = User_School_id.user_id
+    WHERE users.id = $1;", [user.id],
+    pool: DBConnection.ConnectionPool)
+    |> Generic.to_struct_list(School)
+  end
+
+
+
+
+end
